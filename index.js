5f403e4c0d283d42c743f9fde7e8d606e04b88fd
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-05-28 13:59:33 +0600

    heroku pblm solve

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ async function run() {
         // Check Admin route
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const user = await usersCollection.findOne({ email: email });
+            const user = await usersCollection.findOne({ email: email }) || {};
             const isAdmin = user.role === "admin";
             res.send({ admin: isAdmin });
         })
